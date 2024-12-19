1bd7964f3228367ee22dcba66fb5ce48d96bc333
Daniel Wincencik <daniel.wincencik@firma.interia.pl>
2024-12-19 15:21:53 +0100

    Rollback with tests

--- pages/dashboard.py
+++ pages/dashboard.py
@@ -1,7 +1,3 @@
-
-import os
-
-import streamlit.components.v1 as components
 from db.db import Database
 from navigation import make_sidebar
 import streamlit as st
@@ -28,19 +24,3 @@ Your user data:
 )
 for s, d in user_details.__dict__.items():
     st.write(f"{s.capitalize()}: {d} ")
-
-command = st.text_input("grep command")
-
-
-
-if st.button("Run tests"):
-    st.write("Running tests...")
-    os.system(f"cd .. && cd .. && cd app && npx playwright test --grep '{command}'")
-    st.write("Tests passed! 🎉")
-
-port = st.text_input("port")
-
-if st.button("show result tests"):
-    os.system(f"cd .. && cd .. && cd app && npx playwright show-report --port '{port}'")
-    components.iframe("http://localhost:9323", height=600, scrolling=True)
-    st.write("Tests passed! 🎉")
